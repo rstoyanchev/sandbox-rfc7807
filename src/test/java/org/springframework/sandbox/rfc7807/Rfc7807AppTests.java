1de144a062c0850880b14cd1cb64687d3f243fc4
rstoyanchev <rstoyanchev@vmware.com>
2022-05-10 12:23:42 +0100

    Add RestTemplate test

--- src/test/java/org/springframework/sandbox/rfc7807/Rfc7807AppTests.java
+++ src/test/java/org/springframework/sandbox/rfc7807/Rfc7807AppTests.java
@@ -1,12 +1,19 @@
 package org.springframework.sandbox.rfc7807;
 
+import java.util.stream.Collectors;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Mono;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.web.client.RootUriTemplateHandler;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.lang.Nullable;
+import org.springframework.web.client.RestClientResponseException;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 
@@ -18,10 +25,14 @@ class Rfc7807AppTests {
 
 	private WebClient webClient;
 
+	private RestTemplate restTemplate;
+
 	@BeforeEach
-	void setUp(@Autowired WebClient.Builder webClientBuilder) {
+	void setUp() {
 		String baseUrl = "http://localhost:" + this.port;
-		this.webClient = webClientBuilder.baseUrl(baseUrl).build();
+		this.webClient = WebClient.builder().baseUrl(baseUrl).build();
+		this.restTemplate = new RestTemplate();
+		this.restTemplate.setUriTemplateHandler(new RootUriTemplateHandler(baseUrl));
 	}
 
 	@Test
@@ -31,10 +42,32 @@ class Rfc7807AppTests {
 				.bodyToMono(String.class)
 				.doOnNext(System.out::println)
 				.onErrorResume(WebClientResponseException.class, ex -> {
-					System.out.println(ex.getResponseBodyAs(ExtendedProblemDetail.class));
+					ExtendedProblemDetail body = ex.getResponseBodyAs(ExtendedProblemDetail.class);
+					System.out.println(format(ex.getStatusCode(), ex.getHeaders(), body));
 					return Mono.empty();
 				})
 				.block();
 	}
+
+	@Test
+	void missingRequestParamWithRestTemplate() {
+		try {
+			String result = this.restTemplate.getForObject("/greeting", String.class);
+			System.out.println(result);
+		}
+		catch (RestClientResponseException ex) {
+			ExtendedProblemDetail body = ex.getResponseBodyAs(ExtendedProblemDetail.class);
+			System.out.println(format(ex.getStatusCode(), ex.getResponseHeaders(), body));
+		}
+	}
+
+	private String format(HttpStatusCode statusCode, @Nullable HttpHeaders headers, ExtendedProblemDetail body) {
+		return "\nHTTP Status " + statusCode + "\n\n" +
+				(headers != null ? headers : HttpHeaders.EMPTY).entrySet().stream()
+						.map(entry -> entry.getKey() + ":" + entry.getValue())
+						.collect(Collectors.joining("\n")) +
+				"\n\n" + body;
+	}
+
 
 }
