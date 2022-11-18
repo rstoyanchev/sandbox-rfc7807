773a4e65c11368e4ad0d4bf143f7461ecfdc3440
rstoyanchev <rstoyanchev@vmware.com>
2022-11-18 16:06:12 +0000

    Upgrade to GA, use the latest API, and update tests

--- src/main/java/org/springframework/sandbox/rfc7807/WebMvcExceptionHandler.java
+++ src/main/java/org/springframework/sandbox/rfc7807/WebMvcExceptionHandler.java
@@ -1,13 +1,10 @@
 package org.springframework.sandbox.rfc7807;
 
-import java.net.URI;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -20,33 +17,14 @@ public class WebMvcExceptionHandler extends ResponseEntityExceptionHandler {
 
 
 	@Override
-	protected ResponseEntity<Object> handleMissingServletRequestParameter(
-			MissingServletRequestParameterException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-
-		// Customize type and detail for MissingServletRequestParameterException
-
-		ProblemDetail body = ex.getBody()
-				.withType(URI.create("/problem/bad-request"))
-				.withDetail("Expected parameter: " + ex.getParameterName());
-
-		return handleExceptionInternal(ex, body, headers, status, request);
-	}
-
-
-	// More overrides to customize other Spring MVC exceptions + any ErrorResponseException...
-
-
-	@Override
-	protected ResponseEntity<Object> handleExceptionInternal(
-			Exception ex, Object body, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
-
-		// ProblemDetail extended for all exceptions
+	protected ResponseEntity<Object> createResponseEntity(
+			Object body, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
 
 		if (body instanceof ProblemDetail detail) {
-			body = new ExtendedProblemDetail(detail, this.host);
+			detail.setProperty("host", this.host);
 		}
 
-		return super.handleExceptionInternal(ex, body, headers, statusCode, request);
+		return super.createResponseEntity(body, headers, statusCode, request);
 	}
 
 }
--- src/test/java/org/springframework/sandbox/rfc7807/Rfc7807AppTests.java
+++ src/test/java/org/springframework/sandbox/rfc7807/Rfc7807AppTests.java
@@ -1,24 +1,28 @@
 package org.springframework.sandbox.rfc7807;
 
-import java.util.stream.Collectors;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Consumer;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import reactor.core.publisher.Mono;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.web.client.RootUriTemplateHandler;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.lang.Nullable;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.RequestEntity;
 import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class Rfc7807AppTests {
+
+	private static final ParameterizedTypeReference<Map<String, Object>> MAP_TYPE = new ParameterizedTypeReference<>() {};
 
 	@LocalServerPort
 	private int port;
@@ -37,36 +41,37 @@ class Rfc7807AppTests {
 
 	@Test
 	void missingRequestParamWithWebClient() {
-		this.webClient.get().uri("/greeting")
-				.retrieve()
-				.bodyToMono(String.class)
-				.doOnNext(System.out::println)
-				.onErrorResume(WebClientResponseException.class, ex -> {
-					ExtendedProblemDetail body = ex.getResponseBodyAs(ExtendedProblemDetail.class);
-					System.out.println(format(ex.getStatusCode(), ex.getHeaders(), body));
-					return Mono.empty();
-				})
-				.block();
+		testRequestParameterNotPresent(url ->
+				this.webClient.get().uri("/greeting").retrieve().bodyToMono(MAP_TYPE).block());
 	}
 
 	@Test
 	void missingRequestParamWithRestTemplate() {
-		try {
-			String result = this.restTemplate.getForObject("/greeting", String.class);
-			System.out.println(result);
-		}
-		catch (RestClientResponseException ex) {
-			ExtendedProblemDetail body = ex.getResponseBodyAs(ExtendedProblemDetail.class);
-			System.out.println(format(ex.getStatusCode(), ex.getResponseHeaders(), body));
-		}
+		testRequestParameterNotPresent(url ->
+				this.restTemplate.exchange(RequestEntity.get(url).build(), MAP_TYPE));
 	}
 
-	private String format(HttpStatusCode statusCode, @Nullable HttpHeaders headers, ExtendedProblemDetail body) {
-		return "\nHTTP Status " + statusCode + "\n\n" +
-				(headers != null ? headers : HttpHeaders.EMPTY).entrySet().stream()
-						.map(entry -> entry.getKey() + ":" + entry.getValue())
-						.collect(Collectors.joining("\n")) +
-				"\n\n" + body;
+	private static void testRequestParameterNotPresent(Consumer<String> requestExecutor) {
+
+		Map<String, Object> expected = new LinkedHashMap<>();
+		expected.put("type", "about:blank");
+		expected.put("title", "Bad Request");
+		expected.put("status", 400);
+		expected.put("detail", "Required parameter 'name' is missing.");
+		expected.put("instance", "/greeting");
+		expected.put("host", "sandbox-rfc7807.spring.io");
+
+		try {
+			requestExecutor.accept("/greeting");
+		}
+		catch (WebClientResponseException ex) {
+			Map<String, Object> actual = ex.getResponseBodyAs(MAP_TYPE);
+			assertThat(actual).containsExactlyEntriesOf(expected);
+		}
+		catch (RestClientResponseException ex) {
+			Map<String, Object> actual = ex.getResponseBodyAs(MAP_TYPE);
+			assertThat(actual).containsExactlyEntriesOf(expected);
+		}
 	}
 
 }
