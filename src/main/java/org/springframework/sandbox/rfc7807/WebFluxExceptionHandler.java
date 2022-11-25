8743ad859d1a1ef10f648393a3c0768c2db65c42
rstoyanchev <rstoyanchev@vmware.com>
2022-11-25 16:22:37 +0000

    Upgrade to GA, use the latest API, and update tests

--- src/main/java/org/springframework/sandbox/rfc7807/WebFluxExceptionHandler.java
+++ src/main/java/org/springframework/sandbox/rfc7807/WebFluxExceptionHandler.java
@@ -1,37 +1,32 @@
 package org.springframework.sandbox.rfc7807;
 
-import java.net.URI;
+import reactor.core.publisher.Mono;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponseException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.server.MissingRequestValueException;
+import org.springframework.web.reactive.result.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.server.ServerWebExchange;
 
 @ControllerAdvice
-public class WebFluxExceptionHandler {
+public class WebFluxExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@Value("${hostname}")
 	private String host;
 
-	@ExceptionHandler
-	public ResponseEntity<ProblemDetail> handle(ErrorResponseException ex) {
 
-		if (ex instanceof MissingRequestValueException missingValueEx) {
-			ex.getBody()
-					.withType(URI.create("/problem/bad-request"))
-					.withDetail("Expected " + missingValueEx.getLabel() + ": " + missingValueEx.getName());
-		}
-		else {
-			// Customize other types of ResponseStatusException + any ErrorResponseException
+	@Override
+	protected Mono<ResponseEntity<Object>> createResponseEntity(
+			Object body, HttpHeaders headers, HttpStatusCode status, ServerWebExchange exchange) {
+
+		if (body instanceof ProblemDetail detail) {
+			detail.setProperty("host", this.host);
 		}
 
-		ProblemDetail body = ex.getBody();
-		body = new ExtendedProblemDetail(body, this.host);
-
-		return new ResponseEntity<>(body, ex.getHeaders(), ex.getStatusCode());
+		return super.createResponseEntity(body, headers, status, exchange);
 	}
 
 }
